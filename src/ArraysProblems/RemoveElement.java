e6d4119e8db345245972a5048ffd41ef155125f7
Ahmed Khaled <ahmed.khaled.aa123@gmail.com>
2024-11-05 10:05:18 +0200

    Arrays/Remove Element

---
+++ src/ArraysProblems/RemoveElement.java
@@ -0,0 +1,34 @@
+package ArraysProblems;
+
+import java.util.Arrays;
+
+public class RemoveElement
+{
+	/*
+	* Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+	* The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+	* */
+
+	public static void main(String[] args)
+	{
+		int[] nums = {3, 2, 2, 3};
+		int val = 3;
+		System.out.println("nums: " + Arrays.toString(nums));
+		int k = removeElement(nums, val);
+		System.out.println("k = " + k);
+		System.out.println("After Remove " +  val+ " = " + Arrays.toString(nums));
+	}
+
+	public static int removeElement(int[] nums, int val){
+		int countNotEqualVal = 0;
+		for(int i=0; i<nums.length; i++){
+			if(nums[i] != val){
+				int temp = nums[countNotEqualVal];
+				nums[countNotEqualVal] = nums[i];
+				nums[i] = temp;
+				countNotEqualVal++;
+			}
+		}
+		return countNotEqualVal;
+	}
+}
